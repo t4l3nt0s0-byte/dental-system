94651051d5c9cb3584c27d1ab3ede6f7cbc44c31
t4l3nt0s0-byte <t4l3nt0s0@gmail.com>
2026-05-20 01:44:38 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbuZGYBxZIAFwNESRoQMVlr8KlIcCdWEI",
+  authDomain: "dental-add.firebaseapp.com",
+  projectId: "dental-add",
+  storageBucket: "dental-add.firebasestorage.app",
+  messagingSenderId: "1350878692",
+  appId: "1:1350878692:web:1b11da0a68054d9f2bf3f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
