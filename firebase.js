72ee056cc6cf2774caeaaa956b4f2389d6e8018b
t4l3nt0s0-byte <t4l3nt0s0@gmail.com>
2026-05-20 02:53:15 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-// Your web app's Firebase configuration
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBbuZGYBxZIAFwNESRoQMVlr8KlIcCdWEI",
   authDomain: "dental-add.firebaseapp.com",
@@ -13,5 +11,10 @@ const firebaseConfig = {
   appId: "1:1350878692:web:1b11da0a68054d9f2bf3f1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+console.log("Firebase conectado");
+
+export { db };
